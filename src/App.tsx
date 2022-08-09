89e44eec70045e0fe68687e1ad53969dc22061e4
Rodrigo Xavier <rodrfx@gmail.com>
2022-08-09 17:56:10 -0300

    feat: added App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Header } from './components/Header';
+import { Router } from './Router';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export const App = () => {
 	return (
-		<>
-			<div>oi</div>
-		</>
+		<ThemeProvider theme={defaultTheme}>
+			<BrowserRouter>
+				<Header />
+				<Router />
+			</BrowserRouter>
+			<GlobalStyle />
+		</ThemeProvider>
 	);
 };
