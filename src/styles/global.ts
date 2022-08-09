053e40fb579355909b9b55970f2fe9e0bceb2c33
Rodrigo Xavier <rodrfx@gmail.com>
2022-08-09 17:52:10 -0300

    feat: added global styled component

---
+++ src/styles/global.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :focus{
+        outline: 0;
+        box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']};
+        border-radius: 2px;
+    }
+
+    html {
+        font-size: 62.5%;
+    }
+
+    body {
+        background: ${({ theme }) => theme['gray-900']};
+        color: ${({ theme }) => theme['gray-300']};
+        --webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 500;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    a{
+        text-decoration: none;
+    }
+    `;
