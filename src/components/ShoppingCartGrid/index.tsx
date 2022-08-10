100993ef9e19c0ef9a1e7cacdb154138a7318235
Rodrigo Xavier <rodrfx@gmail.com>
2022-08-10 20:17:09 -0300

    conversion to dynamic data

--- src/components/ShoppingCartGrid/index.tsx
+++ src/components/ShoppingCartGrid/index.tsx
@@ -1,12 +1,24 @@
+import { useContext } from 'react';
+import { UserShoppingCart } from '../../context/UserShoppingCart';
 import { ShoppingCartItem } from '../ShoppingCartItem';
 import * as S from './styles';
 
 export const ShoppingCartGrid = () => {
+	const { cart } = useContext(UserShoppingCart);
+
 	return (
 		<S.Wrapper>
-			<ShoppingCartItem />
-			<ShoppingCartItem />
-			<ShoppingCartItem />
+			{cart.map((productCart) => {
+				return (
+					<ShoppingCartItem
+						key={productCart.id}
+						image={productCart.image}
+						price={productCart.price}
+						title={productCart.title}
+						quantity={productCart.quantity}
+					/>
+				);
+			})}
 		</S.Wrapper>
 	);
 };
