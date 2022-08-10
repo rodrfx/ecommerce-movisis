ffed701619db59266cd0d7c16dbcf41b39b00cd4
Rodrigo Xavier <rodrfx@gmail.com>
2022-08-10 20:04:55 -0300

    feat: added UserShoppingCartContextProvider in APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Header } from './components/Header';
+import { UserShoppingCartContextProvider } from './context/UserShoppingCart';
 import { Router } from './Router';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
@@ -8,10 +9,12 @@ import { defaultTheme } from './styles/themes/default';
 export const App = () => {
 	return (
 		<ThemeProvider theme={defaultTheme}>
-			<BrowserRouter>
-				<Header />
-				<Router />
-			</BrowserRouter>
+			<UserShoppingCartContextProvider>
+				<BrowserRouter>
+					<Header />
+					<Router />
+				</BrowserRouter>
+			</UserShoppingCartContextProvider>
 			<GlobalStyle />
 		</ThemeProvider>
 	);
